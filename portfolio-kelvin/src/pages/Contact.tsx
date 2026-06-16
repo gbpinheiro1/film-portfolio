f8c92018c9eae8c70b410d1bbad404e7465bf18b
gbpinheiro1 <gabrielpinheiro551@gmail.com>
2026-06-15 21:56:05 -0300

    style: Contact form style

--- portfolio-kelvin/src/pages/Contact.tsx
+++ portfolio-kelvin/src/pages/Contact.tsx
@@ -1,38 +1,54 @@
+import { NavToMenu } from "../components/NavToMenu"
+import iconSrc from "../assets/menu-icon.svg"
+
 export function Contact() {
   return (
     <div className="flex justify-center items-center h-dvh">
-      <form className=" bg-taupe-100 text-black ">
-        <h1>Contato</h1>
+      <NavToMenu
+        name="Kelvin Andrade"
+        occupation="Produtor Audiovisual"
+        iconSrc={iconSrc}
+      ></NavToMenu>
+
+      <form className=" flex flex-col gap-1 bg-taupe-100 text-black w-2/3 max-w-150 px-6 py-3 rounded-lg font-jost">
+        <h1 className="text-center text-3xl font-bold">Contato</h1>
         <div>
-          <label htmlFor="">Nome Completo</label>
+          <label htmlFor="txt-input">Nome Completo</label>
           <input
             type="text"
-            className="field"
+            id="txt-input"
+            className="w-full h-10 bg-transparent border outline-zinc-300 focus:outline-blue-700 rounded-md p-4 font-inter text-md text-black mt-2"
             placeholder="Digite seu Nome"
             required
           />
         </div>
 
-        <div>
-          <label htmlFor="">Email</label>
+        <div className="mt-3.5">
+          <label htmlFor="email-input">Email</label>
           <input
             type="email"
-            className="field"
+            id="email-input"
+            className="w-full h-10 bg-transparent border outline-zinc-300 focus:outline-blue-700 rounded-md p-4 font-inter text-md text-black mt-2"
             placeholder="Digite seu email"
             required
           />
         </div>
 
-        <div>
-          <label htmlFor="">Mensagem</label>
+        <div className="mt-3.5">
+          <label htmlFor="text-area">Mensagem</label>
           <textarea
-            name=""
-            id=""
+            id="text-area"
             placeholder="Digite sua mensagem"
             required
-            className="field-msg"
+            className="w-full bg-transparent border outline-zinc-300 focus:outline-blue-700 rounded-md p-4 font-inter text-md text-black mt-2 h-48 resize-none"
           ></textarea>
-          <button type="submit">Enviar Mensagem</button>
+
+          <button
+            type="submit"
+            className="w-full h-14 bg-black border-none rounded-md shadow-sm cursor-pointer text-lg text-white font-semibold mt-5 hover:bg-blue-700 transition-colors duration-400"
+          >
+            Enviar Mensagem
+          </button>
         </div>
       </form>
     </div>
