8251871cd17e3c503d11518b0dc3907e52794843
gbpinheiro1 <gabrielpinheiro551@gmail.com>
2026-06-23 19:08:45 -0300

    style: responsiveness adjustments part 5

--- portfolio-kelvin/src/pages/About.tsx
+++ portfolio-kelvin/src/pages/About.tsx
@@ -4,7 +4,7 @@ import portrait from "../assets/foto-retrato.avif"
 
 export function About() {
   return (
-    <div className="flex flex-col w-full h-dvh">
+    <div className="flex flex-col w-full h-dvh not-md:h-full not-md:mb-3">
       <header>
         <NavToMenu
           name="Kelvin Andrade"
@@ -14,16 +14,22 @@ export function About() {
         ></NavToMenu>
       </header>
 
-      <main className="flex flex-row w-full h-full items-center">
-        <div className="flex flex-row justify-evenly w-full">
-          <div className="w-3/10 lg:w-1/4 mt-1">
-            <img src={portrait} alt="" className="w-5/6 rounded-xl" />
+      <main className="flex md:flex-row w-full md:h-full items-center">
+        <div className="flex flex-col md:flex-row md:justify-evenly not-md:items-center gap-7 md:gap-0 w-full">
+          <div className="flex w-6/10 xs:w-1/2 custom:w-4/10 sm:w-35/100 md:w-3/10 lg:w-1/4 mt-4 md:mt-1 not-md:justify-center">
+            <div className="w-5/6 rounded-xl aspect-493/740">
+              <img
+                src={portrait}
+                alt=""
+                className="w-full h-full object-cover rounded-xl"
+              />
+            </div>
           </div>
-          <div className="flex flex-col gap-3 lgscreen:gap-6">
-            <h1 className="md:text-3xl xl:text-4xl 2xl:text-5xl lgscreen:text-6xl font-cinzel">
+          <div className="flex flex-col gap-1 custom:gap-3 lgscreen:gap-6m w-8/10 xs:w-3/4 custom:w-6/10 md:w-fit not-md:items-center not-md:text-justify">
+            <h1 className="text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl lgscreen:text-6xl font-cinzel">
               Sobre
             </h1>
-            <p className="max-w-100 md:max-w-115 lg:max-w-150 xl:max-w-180 2xl:max-w-220 text-base lg:text-lg xl:text-xl 2xl:text-2xl font-josefin font-light lgscreen:max-w-300 lgscreen:text-4xl">
+            <p className="max-w-full md:max-w-115 lg:max-w-150 xl:max-w-180 2xl:max-w-220 text-base lg:text-lg xl:text-2xl 2xl:text-3xl font-josefin font-light lgscreen:max-w-300 lgscreen:text-4xl">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Nesciunt, eum sunt. Optio ex fugiat earum tenetur laboriosam ea?
               Quasi porro accusantium culpa. Eveniet, suscipit dolore
